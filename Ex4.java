75b9ce6f553949dcd7104f245835ddff1104d86d
Aditya Durgapu <adityadurgapu@gmail.com>
2025-12-26 21:49:08 +0530

    Leap year

---
+++ Ex4.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Ex4 {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Year:");
+        int year = obj.nextInt();
+
+        if (year % 400 == 0) {
+            System.out.println(year + " is a leap year.");   
+        } else if (year % 100 == 0) {
+            System.out.println(year + " is not a leap year."); 
+        } else if (year % 4 == 0) {
+            System.out.println(year + " is a leap year.");  
+        } else {
+            System.out.println(year + " is not a leap year."); 
+        }
+    }
+}
